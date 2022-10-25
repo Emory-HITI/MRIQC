d37178a5bcf909b10af1e2c3d793cb5696ef91d9
PraroopChanda <64160432+PraroopChanda@users.noreply.github.com>
2022-10-26 00:24:58 +0530

    Add files via upload

--- Test_High_Contrast_Spatial_Resolution/Row_Column_Extraction.py
+++ Test_High_Contrast_Spatial_Resolution/Row_Column_Extraction.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[2]:
 
 
 import numpy as np
@@ -9,10 +9,32 @@ import cv2
 import math
 import matplotlib.pyplot as plt
 import copy
+import xlwt
+from xlwt import Workbook
 
+# Workbook is created
+wb = Workbook()
 
+sheet1 = wb.add_sheet('Sheet 1')
+
+sheet1.write(0, 1, 'ARRAY1')
+sheet1.write(0, 2, 'ARRAY2')
+sheet1.write(0, 3, 'ARRAY3')
+sheet1.write(1,0,'Row1')
+sheet1.write(2,0,'Row2')
+sheet1.write(3,0,'Row3')
+sheet1.write(4,0,'Row4')
+sheet1.write(5,0,'Col1')
+sheet1.write(6,0,'Col2')
+sheet1.write(7,0,'Col3')
+sheet1.write(8,0,'Col4')
+#sheet1.write(1, 1, num_spokes_9)
+#sheet1.write(2, 1, num_spokes_8)
+#sheet1.write(3, 1, num_spokes_7)
+#sheet1.write(4, 1, num_spokes_6)
 
 def row_col_extraction(outline_image,localized_array,threshold_image,array_number):
+    print("pika")
     length=0
     if (array_number==1):
         arr_num="arr1"
@@ -26,8 +48,7 @@ def row_col_extraction(outline_image,localized_array,threshold_image,array_numbe
             
     M=outline_image.shape[1] ## width of the image
     N=outline_image.shape[0] ## height of the image
-    #dup_localized_array=localized_array.copy() ## creating a copy image to overlay contours
-    dup_localized_array=localized_array.copy()
+    dup_localized_array=localized_array.copy() ## creating a copy image to overlay contours
     pixel_location_hor=[0,0]
     pixel_location_vert=[0,0]
     value_hor=0
@@ -128,7 +149,7 @@ def row_col_extraction(outline_image,localized_array,threshold_image,array_numbe
             pixel_right_hor[0]=math.floor((pixel_right_hor[0]+j)/2)
             break
             
-   # print ("right_right_x value is",pixel_right_hor[1],"pixel y right right value",pixel_right_hor[0])            
+    #print ("right_right_x value is",pixel_right_hor[1],"pixel y right right value",pixel_right_hor[0])            
     ## getting the right side upper value
     pixel_right_vert=[0,0]
     val_r_top=0
@@ -173,13 +194,23 @@ def row_col_extraction(outline_image,localized_array,threshold_image,array_numbe
     top_right_cord=pixel_right_hor[1]+5 #x value
     wid1=top_right_cord-top_left_cord
     hei1=low_left_cord-high_upper_value
-    #cv2.rectangle(localized_array,(top_left_cord,high_upper_value),(top_left_cord+wid,high_upper_value+hei),(255,255,0),2)
-    cv2.rectangle(dup_localized_array,(top_left_cord,high_upper_value),(top_left_cord+wid1,high_upper_value+hei1),(255,255,0),2)
+    ####cv2.rectangle(dup_localized_array,(top_left_cord,high_upper_value),(top_left_cord+wid1,high_upper_value+hei1),(255,255,0),2)
+    
     # cropping out the row
     row_1=np.zeros((hei1,wid1),np.uint8)
     for i in range(0,wid1):
         for j in range(0,hei1):
             row_1[j,i]=localized_array[j+high_upper_value,i+top_left_cord]
+            
+    row_1 = cv2.resize(row_1,None,fx=0.7,fy=0.7,interpolation = cv2.INTER_CUBIC)        
+    num1=rc_object_count(row_1)  
+    if (num1==4):
+        cv2.rectangle(dup_localized_array,(top_left_cord,high_upper_value),(top_left_cord+wid1,high_upper_value+hei1),(255,255,0),2)
+        
+                                           
+#     cv2.imshow("outline_image",row_1)
+#     cv2.waitKey()
+#     cv2.destroyAllWindows()
                                      
     #cropping second and third row subsequently
     for x in range(1,3):
@@ -199,14 +230,25 @@ def row_col_extraction(outline_image,localized_array,threshold_image,array_numbe
             for i in range(0,wid2):
                 for j in range(0,hei2):
                     row_2[j,i]=localized_array[j+high_upper_value,i+top_left_cord]
+            row_2 = cv2.resize(row_2,None,fx=0.7,fy=0.7,interpolation = cv2.INTER_CUBIC)
+            num2=rc_object_count(row_2)
+            if (num2==4):
+                cv2.rectangle(dup_localized_array,(top_left_cord,high_upper_value),(top_left_cord+(mid_value[1]-top_left_cord),high_upper_value+(low_left_cord-high_upper_value)),(255,255,0),2)
+                
+                               
+                           
         elif (x==2):
             row_3=np.zeros((hei2,wid2),np.uint8)
             for i in range(0,wid2):
                 for j in range(0,hei2):
                     row_3[j,i]=localized_array[j+high_upper_value,i+top_left_cord]
-        
+            row_3=cv2.resize(row_3,None,fx=0.7,fy=0.7,interpolation = cv2.INTER_CUBIC)
+            num3=rc_object_count(row_3)
+            if (num3==4):
+                cv2.rectangle(dup_localized_array,(top_left_cord,high_upper_value),(top_left_cord+(mid_value[1]-top_left_cord),high_upper_value+(low_left_cord-high_upper_value)),(255,255,0),2)
                 
-        cv2.rectangle(dup_localized_array,(top_left_cord,high_upper_value),(top_left_cord+(mid_value[1]-top_left_cord),high_upper_value+(low_left_cord-high_upper_value)),(255,255,0),2)        
+        
+                       
         # make it back to initial value
         low_left_cord-=x*length
         high_upper_value-=x*length
@@ -221,6 +263,8 @@ def row_col_extraction(outline_image,localized_array,threshold_image,array_numbe
             mid_value[1]=i
             break
             
+    #print(mid_value[0])
+    #print(mid_value[1])
     ## will crop out the extended fourth row after detection of first coloumn
     ## Done as a measure to always get the correct coordinates
     
@@ -245,7 +289,7 @@ def row_col_extraction(outline_image,localized_array,threshold_image,array_numbe
             last_col[0]=(last_col[0]+j)//2
             break
             
-   # print("last_column x value is",last_col[1],"last_Column y value is",last_col[0])
+    #print("last_column x value is",last_col[1],"last_Column y value is",last_col[0])
     
     # now using these values we will get all the values for the last row
     #getting the center coordinates for last column last row
@@ -268,8 +312,8 @@ def row_col_extraction(outline_image,localized_array,threshold_image,array_numbe
             last_col_first_point=j
             break
             
-    #print(last_center_val[1])
-    #print(last_col_first_point)
+    print(last_center_val[1])
+    print(last_col_first_point)
     
     last_col_start=last_center_val[1]-length//2;
     if (length==45):
@@ -277,19 +321,22 @@ def row_col_extraction(outline_image,localized_array,threshold_image,array_numbe
     hei3=last_point_last_col-last_col_first_point 
     wid3=length 
     #overlaying the coloumn  
-    cv2.rectangle(dup_localized_array,(last_col_start,last_col_first_point),(last_col_start+wid3,last_col_first_point+hei3),(255,255,0),2)
     ##cropping out the coloumn
     col_4=np.zeros((hei3,wid3),np.uint8)
     for i in range(0,wid3):
         for j in range(0,hei3):
             col_4[j,i]=localized_array[j+last_col_first_point,i+last_col_start]
-            
+    col_4 = cv2.resize(col_4,None,fx=0.7,fy=0.7,interpolation = cv2.INTER_CUBIC)
+    ncol4=rc_object_count(col_4)
+    if (ncol4==4):
+        cv2.rectangle(dup_localized_array,(last_col_start,last_col_first_point),(last_col_start+wid3,last_col_first_point+hei3),(255,255,0),2)
+        
+          
     ##applicable only for fourth row
     if (mid_value[1]<last_center_val[1]): ## kind of a precaution step so that it gets the entire combined row(Otherwise might stop due to threshold)
         mid_value[1]=last_center_val[1]+20
         
     ##now cropping overlaying and cropping the combined fourth row
-    #cv2.rectangle(dup_localized_array,(top_left_cord,high_upper_value),(top_left_cord+(mid_value[1]-top_left_cord),high_upper_value+(low_left_cord-high_upper_value)),(255,255,0),2)
     row_extend=[]
     for i in range(0,mid_value[1]-top_left_cord):
         for j in range(0,low_left_cord-high_upper_value):
@@ -318,13 +365,23 @@ def row_col_extraction(outline_image,localized_array,threshold_image,array_numbe
             for i in range(0,wid4):
                 for j in range(0,hei4):
                     col_3[j,i]=localized_array[j+last_col_first_point,i+last_col_start]
+            col_3 = cv2.resize(col_3,None,fx=0.7,fy=0.7,interpolation = cv2.INTER_CUBIC)
+            ncol3=rc_object_count(col_3)
+            if (ncol3==4):
+                cv2.rectangle(dup_localized_array,(last_col_start,last_col_first_point),(last_col_start+wid4,last_col_first_point+hei4),(255,255,0),2) 
+                
         elif(x==2):
             col_2=np.zeros((hei4,wid4),np.uint8)
             for i in range(0,wid4):
                 for j in range(0,hei4):
                     col_2[j,i]=localized_array[j+last_col_first_point,i+last_col_start]
+            col_2 = cv2.resize(col_3,None,fx=0.7,fy=0.7,interpolation = cv2.INTER_CUBIC)
+            ncol2=rc_object_count(col_3)
+            if (ncol2==4):
+                cv2.rectangle(dup_localized_array,(last_col_start,last_col_first_point),(last_col_start+wid4,last_col_first_point+hei4),(255,255,0),2) 
+                
                     
-        cv2.rectangle(dup_localized_array,(last_col_start,last_col_first_point),(last_col_start+wid4,last_col_first_point+hei4),(255,255,0),2) 
+                    
         ##make the value so that it can be used in future references
         last_col_start+=x*length    
         
@@ -347,7 +404,6 @@ def row_col_extraction(outline_image,localized_array,threshold_image,array_numbe
     wid6=length
     hei6=last_point_last_col-last_col_first_point
     #overlaying combined col1
-    #cv2.rectangle(dup_localized_array,(last_col_start,last_col_first_point),(last_col_start+wid6,last_col_first_point+hei6),(255,0,0),2)
     col_extend=[]
     for i in range(0,wid6):
         for j in range(0,hei6):
@@ -359,6 +415,7 @@ def row_col_extraction(outline_image,localized_array,threshold_image,array_numbe
     s_col = set(t_col)
     s_cr=s_row.intersection(s_col)
     Common_elements=list(s_cr)
+    print(len(Common_elements))
     ##Cropping the fourth row by capturing the common elements
     min_x=100000
     min_y=100000
@@ -377,6 +434,8 @@ def row_col_extraction(outline_image,localized_array,threshold_image,array_numbe
     center_val=[0,0]
     center_val[0]=(max_y+min_y)//2
     center_val[1]=(max_x+min_x)//2
+    #print("mid value x is",center_val[1])
+    #print("mid value y is",center_val[0])
    ## extracting the fourth row 
     #iterating in left direction
     fourth_row_start=[0,0]
@@ -400,11 +459,18 @@ def row_col_extraction(outline_image,localized_array,threshold_image,array_numbe
     wid7=max_x-fourth_row_start[1]
     hei7=fourth_row_down_value-min_y
     #overlaying fourth row
-    cv2.rectangle(dup_localized_array,(fourth_row_start[1],min_y),(fourth_row_start[1]+wid7,min_y+hei7),(255,255,0),2)
+    #cv2.rectangle(dup_localized_array,(fourth_row_start[1],min_y),(fourth_row_start[1]+wid7,min_y+hei7),(255,255,0),2)
     row_4=np.zeros((hei7,wid7),np.uint8)
     for i in range(0,wid7):
         for j in range(0,hei7):
             row_4[j,i]=localized_array[j+min_y,i+fourth_row_start[1]]
+    row_4 = cv2.resize(row_4,None,fx=0.7,fy=0.7,interpolation = cv2.INTER_CUBIC)
+    num4=rc_object_count(row_4)
+    if (num4==4):
+        cv2.rectangle(dup_localized_array,(fourth_row_start[1],min_y),(fourth_row_start[1]+wid7,min_y+hei7),(255,255,0),2)
+        
+    
+            
     
     ## going for first coloumn
     first_col_down_val=0
@@ -428,63 +494,105 @@ def row_col_extraction(outline_image,localized_array,threshold_image,array_numbe
             if (threshold_image[j,i]==255):
                 if (i<f_col_x):
                     f_col_x=i
+                    
+    #print("least x value is ",f_col_x)
+    
     if (f_col_x>min_x):
         f_col_x=min_x
         
     if (f_col_x<min_x):
         if (abs(f_col_x-min_x)>8):
             f_col_x+=abs((f_col_x-min_x)//2)
+    
+    
     wid8=max_x-min_x
     hei8=first_col_down_val+10-min_y
     
-    cv2.rectangle(dup_localized_array,(f_col_x,min_y),(f_col_x+wid8,min_y+hei8),(255,255,0),2)
+    #cv2.rectangle(dup_localized_array,(f_col_x,min_y),(f_col_x+wid8,min_y+hei8),(255,255,0),2)
     
     col_1=np.zeros((hei8,wid8),np.uint8)
     for i in range(0,wid8):
         for j in range(0,hei8):
             col_1[j,i]=localized_array[j+min_y,i+f_col_x]
-    
-    cv2.imshow("row1",row_1)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-    cv2.imshow("row2",row_2)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-    cv2.imshow("row3",row_3)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-    cv2.imshow("row4",row_4)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-    #######
-    cv2.imshow("col1",col_1)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-    cv2.imshow("col2",col_2)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-    cv2.imshow("col3",col_3)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-    cv2.imshow("col4",col_4)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    col_1 = cv2.resize(col_1,None,fx=0.7,fy=0.7,interpolation = cv2.INTER_CUBIC)
+    ncol1=rc_object_count(col_1)
+    if (ncol1==4):
+        cv2.rectangle(dup_localized_array,(f_col_x,min_y),(f_col_x+wid8,min_y+hei8),(255,255,0),2)
+        
+            
+    ## uncomment if you want to see individual rows and columns at each step
+#     cv2.imshow("row1",row_1)
+#     cv2.waitKey()
+#     cv2.destroyAllWindows()
+#     cv2.imshow("row2",row_2)
+#     cv2.waitKey()
+#     cv2.destroyAllWindows()
+#     cv2.imshow("row3",row_3)
+#     cv2.waitKey()
+#     cv2.destroyAllWindows()
+#     cv2.imshow("row4",row_4)
+#     cv2.waitKey()
+#     cv2.destroyAllWindows()
+#     #######
+#     cv2.imshow("col1",col_1)
+#     cv2.waitKey()
+#     cv2.destroyAllWindows()
+#     cv2.imshow("col2",col_2)
+#     cv2.waitKey()
+#     cv2.destroyAllWindows()
+#     cv2.imshow("col3",col_3)
+#     cv2.waitKey()
+#     cv2.destroyAllWindows()
+#     cv2.imshow("col4",col_4)
+#     cv2.waitKey()
+#     cv2.destroyAllWindows()    
     
     cv2.imshow("outline_image",dup_localized_array)
     cv2.waitKey()
     cv2.destroyAllWindows()
+
+    Count_list=[num1,num2,num3,num4,ncol1,ncol2,ncol3,ncol4]
+    for x in range(len(Count_list)):
+        if (Count_list[x]==4):
+            Count_list[x]=1
+        else:
+            Count_list[x]=0
+    
+    if (array_number==1):
+        sheet1.write(1, 1, Count_list[0])
+        sheet1.write(2, 1, Count_list[1])
+        sheet1.write(3, 1, Count_list[2])
+        sheet1.write(4, 1, Count_list[3])
+        sheet1.write(5, 1, Count_list[4])
+        sheet1.write(6, 1, Count_list[5])
+        sheet1.write(7, 1, Count_list[6])
+        sheet1.write(8, 1, Count_list[7])
+    elif (array_number==2):
+        sheet1.write(1, 2, Count_list[0])
+        sheet1.write(2, 2, Count_list[1])
+        sheet1.write(3, 2, Count_list[2])
+        sheet1.write(4, 2, Count_list[3])
+        sheet1.write(5, 2, Count_list[4])
+        sheet1.write(6, 2, Count_list[5])
+        sheet1.write(7, 2, Count_list[6])
+        sheet1.write(8, 2, Count_list[7])
+    elif (array_number==3):
+        sheet1.write(1, 3, Count_list[0])
+        sheet1.write(2, 3, Count_list[1])
+        sheet1.write(3, 3, Count_list[2])
+        sheet1.write(4, 3, Count_list[3])
+        sheet1.write(5, 3, Count_list[4])
+        sheet1.write(6, 3, Count_list[5])
+        sheet1.write(7, 3, Count_list[6])
+        sheet1.write(8, 3, Count_list[7])
         
-                                           
-                                           
-                          
-            
+    wb.save('xlwt example8.xls')    
+                                                                                 
 #############################################################            
 
 
-outline_image=cv2.imread(r"C:\Users\praroop_2\Desktop\QC_try\outline_image_arr1.png",0)
-localized_array=cv2.imread(r"C:\Users\praroop_2\Desktop\QC_try\Bicubic_image_arr1.png",0)
-threshold_image=cv2.imread(r"C:\Users\praroop_2\Desktop\QC_try\threshold_image_arr1.png",0)
-array_number=1
+# In[ ]:
 
-row_col_extraction(outline_image,localized_array,threshold_image,array_number)
+
+
 
