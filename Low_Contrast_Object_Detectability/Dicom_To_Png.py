fb6fc7bb87b045c9ca2b6b43a276600372e5529e
PraroopChanda <64160432+PraroopChanda@users.noreply.github.com>
2022-11-06 02:24:58 +0530

    Update_Path

--- Low_Contrast_Object_Detectability/Dicom_To_Png.py
+++ Low_Contrast_Object_Detectability/Dicom_To_Png.py
@@ -8,7 +8,7 @@ import numpy as np
 import pydicom as dicom
 import matplotlib.pyplot as plt
 import cv2
-image_path=r'D:\Dicom_data\3_AX_T1\1.2.840.113619.2.495.11565861.612164.25601.1652700575.109.dcm'
+image_path=r'data\1.2.840.113619.2.495.11565861.612164.25601.1652700575.109.dcm'
 ds=dicom.dcmread(image_path)
 data=ds.pixel_array
 new_image = ds.pixel_array.astype(float)
