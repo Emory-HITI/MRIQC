0cfef118644bf22f38a50cbb8f65b190515e8c59
PraroopChanda <64160432+PraroopChanda@users.noreply.github.com>
2022-11-06 02:52:29 +0530

    variable_name_change

--- Test_High_Contrast_Spatial_Resolution/Dicom_To_Png_HC.py
+++ Test_High_Contrast_Spatial_Resolution/Dicom_To_Png_HC.py
@@ -12,8 +12,8 @@ image_path=r'D:\Dicom_data\3_AX_T1\1.2.840.113619.2.495.11565861.612164.25601.16
 ds=dicom.dcmread(image_path)
 data=ds.pixel_array
 new_image = ds.pixel_array.astype(float)
-scaled_image = (np.maximum(new_image, 0) / new_image.max()) * 255.0
-scaled_image = np.uint8(scaled_image)
+scaled_image_1 = (np.maximum(new_image, 0) / new_image.max()) * 255.0
+scaled_image_1 = np.uint8(scaled_image_1)
 #cv2.imshow("Image",scaled_image)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
